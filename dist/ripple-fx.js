4493ae051eb7c67619c3dce934a13252658b3e28
corsa-dn <linkjkeee@gmail.com>
2018-06-06 16:26:06 +0300

    fixed getter and setter

--- dist/ripple-fx.js
+++ dist/ripple-fx.js
@@ -1,39 +1,65 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-}
-var Ripple = function Ripple(elem, color) {
-  var _this = this;
-  _classCallCheck(this, Ripple);
-  this.$elements = document.querySelectorAll(elem);
-  this.$color = color;
-  this.$elements.forEach(function (el) {
-    el.addEventListener("click", function (e) {
-      el.classList.add("ripple-fx");
-      var wave = document.createElement("div");
-      wave.classList.add("ripple__wave");
-      wave.style.backgroundColor = _this.$color;
-      el.insertBefore(wave, null);
-      var click_pos = {
-        x: e.clientX - el.offsetLeft,
-        y: e.clientY - el.offsetTop
-      };
-      wave.style.top = click_pos.y - wave.clientHeight / 2 + "px";
-      wave.style.left = click_pos.x - wave.clientWidth / 2 + "px";
-      wave.style.animationPlayState = "running";
-      wave.addEventListener("animationend", function () {
-        wave.style.animationPlayState = "stopped";
-        el.removeChild(wave);
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Ripple =
+/*#__PURE__*/
+function () {
+  function Ripple(elem, color) {
+    var _this = this;
+
+    _classCallCheck(this, Ripple);
+
+    // входные параметры: целевой элемент, цвет волны
+    this.$elements = document.querySelectorAll(elem); // выбор целевых элементов DOM
+
+    this.$color = color;
+    this.$elements.forEach(function (el) {
+      el.addEventListener("click", function (e) {
+        // клик на элементе
+        el.classList.add("ripple-fx");
+        var wave = document.createElement("div"); // создаем волну
+
+        wave.classList.add("ripple__wave"); // добавляем ей класс
+
+        wave.style.backgroundColor = _this.$color; // устанавливаем цвет со входных параметров по умолчанию #ffffff - белый
+
+        el.insertBefore(wave, null); // вставляем волну в целевой элемент
+
+        var click_pos = {
+          // собираем координаты клика для правильного позициорования волны
+          x: e.clientX - el.offsetLeft,
+          y: e.clientY - el.offsetTop
+        };
+        wave.style.top = click_pos.y - wave.clientHeight / 2 + "px"; // позиционируем волну внутри кнопки по Y
+
+        wave.style.left = click_pos.x - wave.clientWidth / 2 + "px"; // позиционируем волну внутри кнопки по X
+
+        wave.style.animationPlayState = "running"; // запускаем анимацию
+
+        wave.addEventListener("animationend", function () {
+          // следим, когда анимация закончится
+          wave.style.animationPlayState = "stopped"; // останавливаем анимацию
+
+          el.removeChild(wave); // удаляем волну, которая прошла
+        });
       });
     });
-  });
-  this.getColor = function () {
-    return _this.$color;
-  };
-  this.setColor = function (newColor) {
-    _this.$color = newColor;
-  };
-};
+  }
+
+  _createClass(Ripple, [{
+    key: "color",
+    set: function set(newColor) {
+      this.$color = newColor;
+    },
+    get: function get() {
+      return this.$color;
+    }
+  }]);
+
+  return Ripple;
+}();
